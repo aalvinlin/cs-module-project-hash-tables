d90bc88e2e75ae45432f63b16f5b26d1db1c7b23
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-06-03 18:11:09 -0700

    remove commented code

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -7,23 +7,8 @@ f(a) + f(b) = f(c) - f(d)
 #q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
 
-
 def f(x):
     return x * 4 + 6
-
-# since f(x) = 4x + 6, the equation f(a) + f(b) = f(c) - f(d) simplifies to
-# a + b + d + 3 = c
-
-# coin/integer partitions
-
-# examples show cases where a = d, meaning 2a + b + 3 = c
-# examples also show cases where b = d, meaning a + 2b + 3 = c
-
-# inefficient O(n^2) approach
-
-# store the sums and differences of each (x, y) combination
-# pair_sums = dict()
-# pair_differences = dict()
 
 function_values = dict()
 
@@ -42,21 +27,6 @@ for number1 in q:
 
         pair_sum = f(number1) + f(number2)
         pair_difference = f(number1) - f(number2)
-
-        # y1 = f(number1)
-        # y2 = f(number2)
-
-        # # store the sum for each (x, y) combination
-        # pair_sum = y1 + y2
-
-        # pair_sums[tuple_1_2] = pair_sum
-        # pair_sums[tuple_2_1] = pair_sum
-
-        # # store the difference for each (x, y) combination
-        # pair_difference = y1 - y2
-
-        # pair_differences[tuple_1_2] = pair_difference
-        # pair_differences[tuple_2_1] = -pair_difference
 
         # store the pairs that will generate each sum
         if pair_sum not in sums_to_known_tuples:
@@ -94,8 +64,3 @@ for value in shared_values:
             c, d = difference_tuple
 
             print(f"f({a}) + f({b}) = f({c}) - f({d})    {function_values[a]} + {function_values[b]} = {function_values[c]} - {function_values[d]}")
-
-# print(sums_to_known_tuples, "\n")
-# print(differences_to_known_tuples, "\n")
-
-# print(shared_values)
